a52c87a40cc7f19e5d45ebc0bd9994bd943fc60f
simonsays321 <90815969+simonsays321@users.noreply.github.com>
2022-01-24 02:44:07 +0800

    Add files via upload
    initial commit

---
+++ web.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+      
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        } 
+    });
+
+    var typed = new Typed(".typing", {
+        strings: ["STI", "COLLEGE", "CALOOCAN"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["running", "lifting", "and ", "playing basketball"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    }); 
+});
